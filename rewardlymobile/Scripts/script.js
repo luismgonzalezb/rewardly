444c3a0c9c1a9e6a0dfd74010c38ecb9cde9b1e2
Tom Black <tom.black@rockfishinteractive.com>
2013-01-18 23:44:39 -0600

    login page front end done

--- rewardlymobile/Scripts/script.js
+++ rewardlymobile/Scripts/script.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    //Clear form fields on focus
+    $('input').focus(function () {
+        if ($(this).attr('id') == 'Password') {
+            $(this).val('').get(0).type = 'password';
+        } else {
+            $(this).val('');
+        }
+    });
+
+
+});
